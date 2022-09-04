efe3dbb1bcc98a964889f9bac911583119b580b0
yukinyunn <kyyc.yf0312.29@icloud.com>
2022-09-04 12:04:02 +0900

    added Bubble_sort

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// flagを用いたバブルソート
+int bubbleSort(int A[], int N) {
+    int sw = 0;
+    bool flag = 1; 
+    for (int i=0; flag; i++) {
+        flag = 0;
+        for (int j=N-1; j>=i+1; j--) {
+            if (A[j] < A[j-1]) {
+                swap(A[j], A[j-1]); //隣接要素を交換する
+                flag = 1;
+                sw++;
+            }
+        }
+    }
+    return sw;
+}
+
+int main() {
+    int A[100];
+    int N, sw;
+    cin >> N;
+    for (int i=0; i<N; i++) cin >> A[i];
+
+    sw = bubbleSort(A, N);
+
+    for (int i=0; i<N; i++) {
+        if(i) cout << " ";
+        cout << A[i];
+    }
+    cout << endl;
+    cout << sw << endl;
+
+    return 0;
+}
